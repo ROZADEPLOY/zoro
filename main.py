e70c49c06ebf99591ae2357bc1f8cdce61fc65a5
ROZADEPLOY <pokupatel101010@gmail.com>
2025-05-14 16:45:08 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from flask import Flask, session
 from flask_session import Session
 from app.routes import routes
 from app.database import init_db
+from flask import Flask
+import os
+
+template_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)), "app", "templates")
+app = Flask(__name__, template_folder=template_dir)
 
 app = Flask(__name__)
 app.secret_key = "roza-database"
